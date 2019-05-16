e69bd72f8f24cdba019fd3d740c24924042b3fcc
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-05-16 16:37:51 -0700

    About Me styling set-up and info updated

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -11,14 +11,22 @@ class AboutMe extends React.Component {
   render() {
     return (
       <div className="AboutMe-Container">
+        <div className="AboutMe-Title-Container">
+          <h1 className="AboutMe-Title">ABOUT</h1>
+        </div>
         <div className="AboutMe-Photo-Container">
           <img src={Roxy} className="AboutMe-Photo" />
         </div>
         <div className="AboutMe-Info-Container">
+          <h3 className="Who">Who Is This Guy?</h3>
           <p className="AboutMe-Info">
-            I am a full-stack software engineer with extensive experience in Javascript, Node, React, SQL/NoSQL databases and other modern full-stack frameworks. I truly get excited to wake up everyday and spend hours coding. I know it sounds cheesy, but it is true!  I love the constant challenges that this field provides and I’m always eager to learn new techniques, in order to stay competitive.  I simply enjoy coding and often use my free time to work on personal projects.  Improving the UI/UX, adding new features and just making them all around cooler!
-
-            One characteristic I constantly here from my mentors and peers is that I have incredible work ethic.  I take great pride in my work and will constantly fight to make the projects the best they can be.  I’m excited to work on a team with similar passion and drive.  Furthermore, I want to work on a team with great culture.  It would be amazing to want to go to work everyday because the team enjoys and supports one other.
+            I am a passionate and diligent Full-Stack Software Engineer with extensive experience in Javascript, Node, React, SQL/NoSQL databases and many other modern full-stack frameworks. I truly get excited to wake up everyday and spend hours coding. I know it sounds cheesy, but it is true!  I love the constant challenges that this field provides and I’m always eager to learn new techniques, in order to stay competitive.
+            </p>
+            <p className="AboutMe-Info">
+            Before finding my true passion in coding, I graduated from UCLA with a degree in Environmental Studies.  I have a strong belief that the preservation of our environment needs to be of primary concern and sustainable practices should be utilized by everyone.  I would love to one day use my skills as a software engineer to help achieve environmental sustainability.
+          </p>
+          <p className="AboutMe-Info">
+            I grew up in Lake Tahoe, California, so other interest of mine are sports and several outdoor activities.  I also have a passion for music and music festivals.
           </p>
         </div>
 
