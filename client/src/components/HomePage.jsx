b7fb0dc28fc8305100c8a32786ced8a46d5e58ab
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-05-16 03:47:58 -0700

    Fixes home page info

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -15,9 +15,9 @@ class HomePage extends React.Component {
           <img src={CowiePhoto} alt="" className="Cowie-Photo" />
         </div>
         <div className="Home-Info-Container">
-          <h1>Hello, I'm Ryan Cowie</h1>
-          <h1>I'm a Full-Stack Software Engineer</h1>
-          <h1>Based in San Francisco</h1>
+          <h1>Hello, I'm Ryan Cowie.</h1>
+          <h1>I'm a Full-Stack Software Engineer,</h1>
+          <h1>Based in San Francisco, CA</h1>
         </div>
         <div className="Home-Button-Container">
           <button className="Home-Button">View My Work</button>
